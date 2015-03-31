7dd7a8f3a2b5d2b76729120fea6095d56692d479
VaneyCarolinne <vaneycarolinne.vc@gmail.com>
2015-03-31 17:48:03 -0430

    Organizado

--- DOM_Tree.h
+++ DOM_Tree.h
@@ -10,65 +10,62 @@ class DOM_Tree
 	private:
 	//atributos
 		Node *First;
-	//Métodos:	
+	//Métodos:	(Privados)
+		//Métodos de Inspección:
+	    void buscar(Element e,Node *aux,Node &found);
+		//Métodos de Modificación
 		Node *copiar(Node *p);
 		void destruir(Node *apRaiz);
-	public:
-		void buscar(Element e,Node *aux,Node &found);
-	public :
+	public : // (Públicos)
+		//Constructores:
 		DOM_Tree():First(NULL){}
 		DOM_Tree(Element e);
 		DOM_Tree(const DOM_Tree &copying);
-		DOM_Tree& operator=(const DOM_Tree &orig);
 		DOM_Tree(Element parameter,list< DOM_Tree > x);
+		//Metodos de Inspeccion:
 		DOM_Tree childNode(int pos);
+		DOM_Tree getElementByID(Element e);
+		//Metodos de Modificacion:
 		void appendChild(int pos,DOM_Tree a);
 		void appendChild(DOM_Tree a);
 		void removeChild(int pos);
 		void replaceChild(int pos, DOM_Tree &subArbol);	
-		DOM_Tree getElementByID(Element e);
-		~DOM_Tree();//Destruye el árbol. 	
+		//Sobrecarga de operadores:
+		DOM_Tree& operator=(const DOM_Tree &orig);
+		//Destructor: 
+		~DOM_Tree();	
 }; 
+/******************************************/
+/********Constructores de la Clase:*******/
+/*****************************************/
 DOM_Tree::DOM_Tree(Element e)
 {	
 	First= new Node(e,NULL,NULL);
 }
-Node * DOM_Tree::copiar(Node *p)
+DOM_Tree::DOM_Tree(Element parameter,list< DOM_Tree > x)
 {
-	if(p!=NULL)
-	{
-		return(new Node(p->element(),copiar(p->firstChild()),copiar(p->nextSibling())));
-		
-	}else
-		{
-			return (NULL);
-		}
 	
-}
-void DOM_Tree::replaceChild(int pos, DOM_Tree &subArbol)
-{
-	int i;
-	Node* aux,*aux2;
-	aux=First->firstChild();
-	aux2=aux->nextSibling();
-	if(pos==1)
-	{
-		aux->setNextSibling(NULL);
-		First->setFirstChild(subArbol.First);
-		subArbol.First->setNextSibling(aux2);
-		destruir(aux);	
-	}else{
-		for(i=2;i<pos;i++)
-		{
-			aux=aux2;
-			aux2=aux2->nextSibling();
+	Node *creado,*a;
+		
+		if(!x.empty()){
+			First= new  Node(parameter);
+			creado=copiar(x.front().First);
+			First->setFirstChild(creado);
+			x.pop_front();
+			a=creado;
+			while(!x.empty())
+			{
+				
+				creado=copiar(x.front().First);
+				a->setNextSibling(creado);
+				x.pop_front();
+				a=creado;
+			}
 		}
-		aux->setNextSibling(subArbol.First);
-		subArbol.First->setNextSibling(aux2->nextSibling());
-		aux2->setNextSibling(NULL);
-		destruir(aux2);
-	}		
 }
+/*****************************/
+/***Métodos de Inspección:****/
+/*****************************/
 DOM_Tree DOM_Tree::childNode(int pos){
 	Node *aux,*aux2;
 	DOM_Tree a;
@@ -86,7 +83,32 @@ DOM_Tree DOM_Tree::childNode(int pos){
 	}
 	return a;
 }
-
+DOM_Tree DOM_Tree::getElementByID(Element e)
+{
+	DOM_Tree r;
+	Node *aux;
+	aux=NULL;
+	buscar(e,First,*aux);
+	r.First=copiar(aux);
+	return (r);
+}
+void DOM_Tree::buscar(Element e,Node *aux,Node &found)
+{
+	if(aux!=NULL)
+	{
+		if(e==aux->element())
+		{
+			found=*aux;
+		}
+		else{
+			buscar(e,aux->firstChild(),found);
+			buscar(e,aux->nextSibling(),found);	
+		}	
+	}	
+}
+/*****************************/
+/****Métodos Modificadores:***/
+/*****************************/
 void DOM_Tree::appendChild(int pos,DOM_Tree a){
 	
 	Node *aux,*aux2,*aux3;
@@ -126,27 +148,41 @@ void DOM_Tree::appendChild(DOM_Tree a){
 		aux->setFirstChild(aux2);
 	}
 }
-/********************************destructor *********************************/
-
-void DOM_Tree::destruir(Node *apRaiz)
+Node * DOM_Tree::copiar(Node *p)
 {
-	// Recorrido en Postorden
-	if (apRaiz != NULL)
+	if(p!=NULL)
 	{
-		destruir(apRaiz->firstChild());
-		destruir(apRaiz->nextSibling());
-		delete apRaiz;
-		apRaiz = NULL;
-	}
+		return(new Node(p->element(),copiar(p->firstChild()),copiar(p->nextSibling())));
+		
+	}else
+		{
+			return (NULL);
+		}	
 }
-
-DOM_Tree::~DOM_Tree()
-{	
-	if(First!=NULL){
-		destruir(First);
-	}
+void DOM_Tree::replaceChild(int pos, DOM_Tree &subArbol)
+{
+	int i;
+	Node* aux,*aux2;
+	aux=First->firstChild();
+	aux2=aux->nextSibling();
+	if(pos==1)
+	{
+		aux->setNextSibling(NULL);
+		First->setFirstChild(subArbol.First);
+		subArbol.First->setNextSibling(aux2);
+		destruir(aux);	
+	}else{
+		for(i=2;i<pos;i++)
+		{
+			aux=aux2;
+			aux2=aux2->nextSibling();
+		}
+		aux->setNextSibling(subArbol.First);
+		subArbol.First->setNextSibling(aux2->nextSibling());
+		aux2->setNextSibling(NULL);
+		destruir(aux2);
+	}		
 }
-
 void DOM_Tree::removeChild(int pos)
 {
 	Node *aux,*aux2;
@@ -178,6 +214,20 @@ void DOM_Tree::removeChild(int pos)
 		cout << "Se intento eliminar un SubArbol Inexistente" <<std::endl;
 	}	
 }
+void DOM_Tree::destruir(Node *apRaiz)
+{
+	// Recorrido en Postorden
+	if (apRaiz != NULL)
+	{
+		destruir(apRaiz->firstChild());
+		destruir(apRaiz->nextSibling());
+		delete apRaiz;
+		apRaiz = NULL;
+	}
+}
+/*****************************/
+/**Sobrecarga de Operadores:**/
+/*****************************/
 DOM_Tree& DOM_Tree::operator=(const DOM_Tree&orig)
 {
 		if (this != &orig)
@@ -186,57 +236,13 @@ DOM_Tree& DOM_Tree::operator=(const DOM_Tree&orig)
 		}
 		return *this;
 }
-DOM_Tree::DOM_Tree(Element parameter,list< DOM_Tree > x)
-{
-	
-	Node *creado,*a;
-		
-		if(!x.empty()){
-			First= new  Node(parameter);
-			creado=copiar(x.front().First);
-			First->setFirstChild(creado);
-			x.pop_front();
-			a=creado;
-			while(!x.empty())
-			{
-				
-				creado=copiar(x.front().First);
-				a->setNextSibling(creado);
-				x.pop_front();
-				a=creado;
-			}
-		}
-
-
-}
-void DOM_Tree::buscar(Element e,Node *aux,Node &found)
-{
-	if(aux!=NULL)
-	{
-		if(e==aux->element())
-		{
-			found=*aux;
-		}
-		else{
-			buscar(e,aux->firstChild(),found);
-			buscar(e,aux->nextSibling(),found);
-			
-			
-		}
-		
+/*********************************/
+/**Único Destructor de la Clase:**/
+/*********************************/
+DOM_Tree::~DOM_Tree()
+{	
+	if(First!=NULL){
+		destruir(First);
 	}
-	
 }
-
-DOM_Tree DOM_Tree::getElementByID(Element e)
-{
-	DOM_Tree r;
-	Node *aux;
-	aux=NULL;
-	buscar(e,First,*aux);
-	r.First=copiar(aux);
-	return (r);
-	
-}
-
 #endif
