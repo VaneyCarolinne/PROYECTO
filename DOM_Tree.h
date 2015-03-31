0a7c79af084ec90de543cc61ae3146548f29c7bf
Sebastian Otero <sebastian.otero1993@gmail.com>
2015-03-31 09:33:08 -0430

    Por aqui pasó Sebastián

--- DOM_Tree.h
+++ DOM_Tree.h
@@ -1,30 +1,86 @@
 #ifndef DOM_H
 #define DOM_H
+
+#include <iostream>
 #include <list>
-#include<iostream>
+#include "Element.h"
 #include "Node.h"
-#include "element.h"
+
 using std::list;
+
 class DOM_Tree
 {
 	private:
-		
-		
 		Node *First;
 		Node *copiar(Node *p);
 		void destruir(Node *apRaiz);
-	public :
+	public:
 		DOM_Tree();
 		DOM_Tree(const DOM_Tree &copying);
 		DOM_Tree(Element parameter,list< DOM_Tree > x);
-		Element childNode(int pos);	
-		void appendChild(int pos , DOM_Tree adding);
+		DOM_Tree childNode(int pos);	
+		void appendChild(int pos,DOM_Tree a);
+		void appendChild(DOM_Tree a);
 		void removeChild(int pos);	
 		DOM_Tree getElementByID();
-		~DOM_Tree();//Destruye el árbol. 
-			
-	};
+		~DOM_Tree();//Destruye el árbol. 	
+};
 
+DOM_Tree DOM_Tree::childNode(int pos){
+	Node *aux,*aux2;
+	DOM_Tree a;
+	int p=1;
+	aux=First;
+	aux=aux->firstChild();
+	while(p<pos && aux->nextSibling()!=NULL){
+		p++;
+		aux=aux->nextSibling();
+	}
+	if(p==pos){
+		aux2=aux->nextSibling();
+		aux->setNextSibling(NULL);
+		a.First=copiar(aux);
+	}
+	return a;
+}
+
+void DOM_Tree::appendChild(int pos,DOM_Tree a){
+	Node *aux,*aux2,*aux3;
+	int p=1;
+	aux=First;
+	aux2=a.First;
+	if(pos==1 && aux->firstChild()==NULL){
+		aux->setFirstChild(aux2);
+	}
+	else{
+		aux=aux->firstChild();
+		while(p<pos && aux->nextSibling()!=NULL){
+			aux3=aux;
+			aux=aux->nextSibling();
+			p++;
+		}
+		if(p==pos){
+			aux3->setNextSibling(aux2);
+			aux2->setNextSibling(aux);
+		}
+	}
+}
+
+void DOM_Tree::appendChild(DOM_Tree a){
+	Node *aux,*aux2;
+	aux=First;
+	aux2=a.First;
+	if(aux->firstChild()==NULL){
+		aux->setFirstChild(aux2);
+	}
+	else{
+		aux=aux->firstChild();
+		while(aux->nextSibling()!=NULL){
+			aux=aux->nextSibling();
+		}
+		aux->setFirstChild(aux2);
+	}
+}
 
 Node * DOM_Tree::copiar(Node *p)
 {
@@ -58,6 +114,7 @@ DOM_Tree::~DOM_Tree()
 		destruir(First);
 	}
 }
+
 void DOM_Tree::removeChild(int pos)
 {
 	Node *aux,*aux2;
